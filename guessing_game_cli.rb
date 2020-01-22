bfd7274b97cde4fc52f6e7d8fdad2db3d0009290
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-22 19:01:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,5 +2,11 @@ def run_guessing_game
   random_numer = rand(6)+11
   puts "Guess a number:"
   input gets.chomp
-  
+  if input == rando.to_s
+    puts "You guessed the correct number!"
+    elsif input.downcase == "exit"
+      puts "Goodbye!"
+    else
+      puts "Sorry"
+    end
 end
