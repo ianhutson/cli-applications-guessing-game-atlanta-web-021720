f7b36db79dc45bae96b200798ddfb2c82d24f60e
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-22 19:06:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   rando = rand(6)+1
   puts "Guess a number between 1 and 6:"
-  input gets.chomp
+  input = gets.chomp
   if input == rando.to_s
     puts "You guessed the correct number!"
     elsif input.downcase == "exit"
