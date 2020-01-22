6f5324d066cbee87604c79d2869bedee2f4990b7
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-22 19:05:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   random_number = rand(6)+1
-  puts "Guess a number:"
+  puts "Guess a number between 1 and 6:"
   input gets.chomp
   if input == rando.to_s
     puts "You guessed the correct number!"
